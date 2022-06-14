ed1cc820ff1c3e306eac15c83c4be6160c12702f
dameyerdave <dameyerdave@gmail.com>
2022-06-14 09:54:31 +0200

    introduced an entrypoint script to run the service by initially download the disgenet data

---
+++ app/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+./manage.py collectstatic --noinput && \
+./manage.py download_data && \
+gunicorn --reload app.asgi:application -k uvicorn.workers.UvicornWorker -w 8 -b 0.0.0.0:8000
