47c9b202f5921f9ac0a2e1f3d18b9f72efb4aa53
dameyerdave <dameyerdave@gmail.com>
2022-06-30 13:24:46 +0200

    Make download_data command more talkative and removed redunant code.

--- app/api/management/commands/download_data.py
+++ app/api/management/commands/download_data.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 from contextlib import closing
 
 DB_FILE = '/data/disgenet.db'
-TSV_FILE = '/data/disease_attr.tsv'
+UMLS_FILE = '/data/disease_attr.tsv'
 
 
 class TqdmUpTo(tqdm):
@@ -26,69 +26,75 @@ def get_creds():
         return f"Basic {creds.read().encode('utf-8')}"
 
 
-def download_db():
-    url = environ.get(
-        'DISGENET_DB_URL', 'https://www.disgenet.org/static/disgenet_ap1/files/sqlite_downloads/current/disgenet_2020.db.gz')
-    url_umls = environ.get(
-        'DISGENET_UMLS_URL', 'https://www.disgenet.org/static/disgenet_ap1/files/downloads/disease_mappings_to_attributes.tsv.gz')
-    gz_file = join('/tmp', basename(urlparse(url).path))
-    gz_file_umls = join('/tmp', basename(urlparse(url_umls).path))
+def download_from_disgenet(url, target, overwrite=False):
+    if overwrite or not exists(target):
+        gz_file = join('/tmp', basename(urlparse(url).path))
+        try:
+            opener = build_opener()
+            opener.addheaders = [('User-Authorization', get_creds())]
+            install_opener(opener)
+            log.info(f"Downloading {target} from {url}...")
+            with TqdmUpTo(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc=gz_file) as t:
+                urlretrieve(url,
+                            filename=gz_file,
+                            reporthook=t.update_to)
+            log.info(f"Gunzip {gz_file} to {target}...")
+            with gzip.open(gz_file, 'rb') as gzf:
+                with open(target, 'wb') as dbf:
+                    shutil.copyfileobj(gzf, dbf)
+            remove(gz_file)
+            log.info(f"DONE downloading {target}.")
+            # We return true if we downloaded something
+            return True
+        except Exception as ex:
+            log.error(ex)
+    # If the file is not downloaded
+    return False
+
+
+def update_db(db_file):
+    log.info(f"Updating db ({db_file})...")
     try:
-        opener = build_opener()
-        opener.addheaders = [('User-Authorization', get_creds())]
-        install_opener(opener)
-        with TqdmUpTo(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc=gz_file) as t:
-            urlretrieve(url,
-                        filename=gz_file,
-                        reporthook=t.update_to)
-        with gzip.open(gz_file, 'rb') as gzf:
-            with open(DB_FILE, 'wb') as dbf:
-                shutil.copyfileobj(gzf, dbf)
-        remove(gz_file)
-
-        with TqdmUpTo(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc=gz_file) as t:
-            urlretrieve(url_umls,
-                        filename=gz_file_umls,
-                        reporthook=t.update_to)
-        with gzip.open(gz_file_umls, 'rb') as gzf_umls:
-            with open(TSV_FILE, 'wb') as tsv_file:
-                shutil.copyfileobj(gzf_umls, tsv_file)
-        remove(gz_file_umls)
+        with sqlite3.connect(db_file) as connection:
+            with closing(connection.cursor()) as cursor:
+                cursor.execute('''UPDATE variantDiseaseNetwork
+                        SET year = null
+                        WHERE year = 'NA';''')
+                connection.commit()
+        log.info(f"DONE updating db ({db_file}).")
     except Exception as ex:
-        log.error(ex)
+        log.error(f"Error during db update: {ex}")
 
 
-def update_db():
-    with sqlite3.connect(DB_FILE) as connection:
-        with closing(connection.cursor()) as cursor:
-            cursor.execute('''UPDATE variantDiseaseNetwork
-                    SET year = null
-                    WHERE year = 'NA';''')
-            connection.commit()
-
-
-def create_new_table():
-    with open(TSV_FILE, 'r') as f:
-        new_data = [tuple(map(lambda x: x.strip(), line.split('\t')))
-                    for line in f.readlines()[1:]]
-    with sqlite3.connect(DB_FILE) as connection:
-        with closing(connection.cursor()) as cursor:
-            cursor.execute('''CREATE TABLE umls (diseaseId text, name text,	type text,
-            	diseaseClassMSH text, diseaseClassNameMSH text, 
-            	 hpoClassId text,	hpoClassName text,
-            	  doClassId text, doClassName text, umlsSemanticTypeId text, umlsSemanticTypeName text);
-            	  
-            	  ''')
-            cursor.executemany(
-                'INSERT INTO umls VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', new_data)
-            connection.commit()
+def create_umls_table(umls_file):
+    log.info(f"Create new table from umls ({umls_file})...")
+    try:
+        with open(umls_file, 'r') as f:
+            new_data = [tuple(map(lambda x: x.strip(), line.split('\t')))
+                        for line in f.readlines()[1:]]
+        with sqlite3.connect(DB_FILE) as connection:
+            with closing(connection.cursor()) as cursor:
+                cursor.execute('''CREATE TABLE umls (diseaseId text, name text,	type text,
+                    diseaseClassMSH text, diseaseClassNameMSH text, 
+                    hpoClassId text,	hpoClassName text,
+                    doClassId text, doClassName text, umlsSemanticTypeId text, umlsSemanticTypeName text);
+                    
+                    ''')
+                cursor.executemany(
+                    'INSERT INTO umls VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', new_data)
+                connection.commit()
+        log.info(f"DONE creating umls table.")
+    except Exception as ex:
+        log.error(f"Error during table umls creation: {ex}")
 
 
 class Command(BaseCommand):
     help = 'run in order to get the db from the disgenet website'
 
     def handle(self, *args, **kwargs):
-        if not exists(DB_FILE):
-            download_db()
-            update_db()
-            create_new_table()
+        if download_from_disgenet(environ.get(
+                'DISGENET_DB_URL', 'https://www.disgenet.org/static/disgenet_ap1/files/sqlite_downloads/current/disgenet_2020.db.gz'), DB_FILE):
+            update_db(DB_FILE)
+        if download_from_disgenet(environ.get(
+                'DISGENET_UMLS_URL', 'https://www.disgenet.org/static/disgenet_ap1/files/downloads/disease_mappings_to_attributes.tsv.gz'), UMLS_FILE):
+            create_umls_table(UMLS_FILE)
