345b3f25b950f64ab7c938a0fd6f5d5bca3f215c
dameyerdave <dameyerdave@gmail.com>
2022-06-30 12:26:05 +0200

    changed the detault port to 9077

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
 ./manage.py collectstatic --noinput && \
+./manage.py makemigrations app && \
+./manage.py migrate && \
 ./manage.py download_data && \
 gunicorn --reload app.asgi:application \
   --log-file - \
   -k uvicorn.workers.UvicornWorker \
   -w 8 \
-  -b 0.0.0.0:8000
+  -b 0.0.0.0:9077
