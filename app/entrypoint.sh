eca0c96eb6544e30de2e324a49eed6f0f1c083b5
dameyerdave <dameyerdave@gmail.com>
2022-07-04 16:58:29 +0200

    introduced a DEV env variable to support dev server startup in development environment

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env sh
 
+PORT=9077
+
 ./manage.py collectstatic --noinput && \
-./manage.py download_data && \
-gunicorn --reload app.asgi:application \
-  --log-file - \
-  -k uvicorn.workers.UvicornWorker \
-  -w 8 \
-  -b 0.0.0.0:9077
+./manage.py download_data
+
+if [ "$DEV" == "True" ]; then
+  ./manage.py runserver 0.0.0.0:${PORT}
+else
+  gunicorn app.asgi:application \
+    --log-file - \
+    -k uvicorn.workers.UvicornWorker \
+    -w 8 \
+    -b 0.0.0.0:${PORT}
+fi
+
